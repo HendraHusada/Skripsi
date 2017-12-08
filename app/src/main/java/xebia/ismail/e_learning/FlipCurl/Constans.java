0295bebed1d25a65406efb3816b824261ca77f30
leviaran <randy.arba@gmail.com>
2017-12-08 22:50:00 +0700

    Add flipcurl

---
+++ app/src/main/java/xebia/ismail/e_learning/FlipCurl/Constans.java
@@ -0,0 +1,16 @@
+package xebia.ismail.e_learning.FlipCurl;
+
+/**
+ * Created by Randy Arba on 12/1/17.
+ * This apps contains Room_persistance
+ *
+ * @email randy.arba@gmail.com
+ * @github https://github.com/Leviaran
+ */
+
+public class Constans {
+        public final static String PREF_MESH_PIXELS = "MeshPixels";
+        public final static String PREF_DURATION    = "Duration";
+        public final static String PREF_PAGE_MODE   = "PageMode";
+
+}
---
+++ app/src/main/java/xebia/ismail/e_learning/FlipCurl/LoadBitmapTask.java
@@ -0,0 +1,257 @@
+package xebia.ismail.e_learning.FlipCurl;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Log;
+import xebia.ismail.e_learning.R;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ * Created by Randy Arba on 12/1/17.
+ * This apps contains Room_persistance
+ *
+ * @email randy.arba@gmail.com
+ * @github https://github.com/Leviaran
+ */
+
+public final class LoadBitmapTask implements Runnable {
+
+    private final static String TAG = "LoadBitmapTask";
+    private static LoadBitmapTask __object;
+
+    final static int SMALL_BG = 0;
+    final static int MEDIUM_BG = 1;
+    final static int LARGE_BG = 2;
+    final static int BG_COUNT = 10;
+
+    int mBGSizeIndex;
+    int mQueueMaxSize;
+    int mPreRandomNo;
+    boolean mIsLandscape;
+    boolean mStop;
+    Random mBGRandom;
+    Resources mResources;
+    Thread mThread;
+    LinkedList<Bitmap> mQueue;
+    int[][] mPortraitBGs;
+    int i = 0;
+
+    /**
+     * Get an unique task object
+     *
+     * @param context Android context
+     * @return unique task object
+     */
+    public static LoadBitmapTask get(Context context) {
+        if (__object == null) {
+            __object = new LoadBitmapTask(context);
+        }
+        return __object;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param context Android context
+     */
+    private LoadBitmapTask(Context context) {
+        mResources = context.getResources();
+        mBGRandom = new Random();
+        mBGSizeIndex = SMALL_BG;
+        mStop = false;
+        mThread = null;
+        mPreRandomNo = 0;
+        mIsLandscape = false;
+        mQueueMaxSize = 1;
+        mQueue = new LinkedList<Bitmap>();
+
+        // init all available bitmaps
+        mPortraitBGs = new int[][] {
+                new int[] {R.drawable.hukum_480, R.drawable.keutamaan_480, R.drawable.rukun_480,
+                        R.drawable.syarat_480},
+                new int[] {R.drawable.hukum_720, R.drawable.keutamaan_720, R.drawable.rukun_720,
+                        R.drawable.syarat_720},
+                new int[] {R.drawable.hukum_720, R.drawable.keutamaan_720,
+                        R.drawable.rukun_720, R.drawable.syarat_720}
+        };
+    }
+
+    /**
+     * Acquire a bitmap to show
+     * <p>If there is no cached bitmap, it will load one immediately</p>
+     *
+     * @return bitmap
+     */
+    public Bitmap getBitmap(int number) {
+        Bitmap b = null;
+        synchronized (this) {
+            if (mQueue.size() > 0) {
+                b = mQueue.pop();
+            }
+
+            notify();
+        }
+
+        if (b == null) {
+            Log.d(TAG, "Load bitmap instantly!");
+            b = getRandomBitmap(number);
+        }
+
+        return b;
+    }
+
+    /**
+     * Is task running?
+     *
+     * @return true if task is running
+     */
+    public boolean isRunning() {
+        return mThread != null && mThread.isAlive();
+    }
+
+    /**
+     * Start task
+     */
+    public synchronized void start() {
+        if (mThread == null || !mThread.isAlive()) {
+            mStop = false;
+            mThread = new Thread(this);
+            mThread.start();
+        }
+    }
+
+    /**
+     * Stop task
+     * <p>Set mStop flag with true and notify task thread, at last, it will
+     * check if task is alive every 500ms with 3 times to make sure the thread
+     * stop</p>
+     */
+    public void stop() {
+        synchronized (this) {
+            mStop = true;
+            notify();
+        }
+
+        // wait for thread stopping
+        for (int i = 0; i < 3 && mThread.isAlive(); ++i) {
+            Log.d(TAG, "Waiting thread to stop ...");
+            try {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e) {
+
+            }
+        }
+
+        if (mThread.isAlive()) {
+            Log.d(TAG, "Thread is still alive after waited 1.5s!");
+        }
+    }
+
+    /**
+     * Set bitmap width , height and maximum size of cache queue
+     *
+     * @param w width of bitmap
+     * @param h height of bitmap
+     * @param maxCached maximum size of cache queue
+     */
+    public void set(int w, int h, int maxCached) {
+        int newIndex = LARGE_BG;
+        if ((w <= 480 && h <= 854) ||
+                (w <= 854 && h <= 480)) {
+            newIndex = SMALL_BG;
+        }
+        else if ((w <= 800 && h <= 1280) ||
+                (h <= 800 && w <= 1280)) {
+            newIndex = MEDIUM_BG;
+        }
+
+        mIsLandscape = w > h;
+
+        if (maxCached != mQueueMaxSize) {
+            mQueueMaxSize = maxCached;
+        }
+
+        if (newIndex != mBGSizeIndex) {
+            mBGSizeIndex = newIndex;
+            synchronized (this) {
+                cleanQueue();
+                notify();
+            }
+        }
+    }
+
+    /**
+     * Load bitmap from resources randomly
+     *
+     * @return bitmap object
+     */
+    private Bitmap getRandomBitmap(int number) {
+        int newNo = mPreRandomNo;
+        newNo = number - 1;
+//        while (newNo == mPreRandomNo) {
+////            newNo = mBGRandom.nextInt(BG_COUNT);
+//            newNo = i++;
+//        }
+
+
+
+        Log.e("currentNumb",String.valueOf(newNo));
+        mPreRandomNo = newNo;
+        int resId = mPortraitBGs[mBGSizeIndex][newNo];
+        Bitmap b = BitmapFactory.decodeResource(mResources, resId);
+        if (mIsLandscape) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap lb = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(),
+                    matrix, true);
+            b.recycle();
+            return lb;
+        }
+
+        return b;
+    }
+
+    /**
+     * Clear cache queue
+     */
+    private void cleanQueue() {
+        for (int i = 0; i < mQueue.size(); ++i) {
+            mQueue.get(i).recycle();
+        }
+        mQueue.clear();
+    }
+
+    public void run() {
+        while (true) {
+            synchronized (this) {
+                // check if ask thread stopping
+                if (mStop) {
+                    cleanQueue();
+                    break;
+                }
+
+                // load bitmap only when no cached bitmap in queue
+                int size = mQueue.size();
+                if (size < 1) {
+                    for (int i = 0; i < mQueueMaxSize; ++i) {
+                        Log.d(TAG, "Load Queue:" + i + " in background!");
+//                        mQueue.push(getRandomBitmap());
+                    }
+                }
+
+                // wait to be awaken
+                try {
+                    wait();
+                }
+                catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
